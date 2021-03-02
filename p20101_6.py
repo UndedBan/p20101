7e5a198b88e4e51b3267c79f6a7803391fdd37ab
UndedBan <79803875+UndedBan@users.noreply.github.com>
2021-03-02 15:20:01 +0200

    Update p20101_6.py

--- p20101_6.py
+++ p20101_6.py
@@ -1,9 +1,9 @@
 import tweepy
 
-ACCESS_TOKEN =
-ACCESS_SECRET =
-CONSUMER_KEY =
-CONSUMER_SECRET =
+ACCESS_TOKEN = 
+ACCESS_SECRET = 
+CONSUMER_KEY =  
+CONSUMER_SECRET =  
 
 
 def connect_to_twitter_OAuth():
@@ -15,38 +15,29 @@ def connect_to_twitter_OAuth():
 
 api = connect_to_twitter_OAuth()
 
-#project start
-xrhsths = input("xrhsths:")
+user = input("User:")
 word_list=[]
-persontweets = api.xrhsths_timeline(screen_name= xrhsths, count=10, include_rts = False)
+persontweets = api.user_timeline(screen_name= user, count=10, include_rts = False)
 for tweet in persontweets:
     word_list.append(tweet.text)
-
 words=[]
+
+#Spaei tis lekseis se lista gia na ginei eukola to sort
 for tweet in word_list:
-    cracked = tweet.split()
-    words = words + cracked
+    splited = tweet.split()
+    words = words + splited
 
-def Sorting(words):
-    words.sort(key=len)
-    return words
+#Sunarthsh gia na taksinomei thn lista me tis lekseis me vash to length
+def Taksinomhsh(words):
+  words.sort(key=len)
+  return words
 
-print(Sorting(words))
+taksinomhmenes = Taksinomhsh(words)
 
-SYMBOLS = '{}()[].,:;+-*/&|_#!@$%^><\?`=~'
-
-results = []
-for element in lekseis:
-    temp = ""
-    for ch in element:
-        if ch not in SYMBOLS:
-            temp += ch
-
-    results.append(temp)
-
-
+#Print ta prwta 5 min ths listas
 for i in range(5):
-    print(results[i])
+  print(taksinomhmenes[i])
 
-for j in reversed(range(len(results)-5,len(results))):
-    print(results[j])
+#Print ta prwta 5 max ths listas
+for j in range((len(words)-5), len(words)):
+  print(taksinomhmenes[j])
